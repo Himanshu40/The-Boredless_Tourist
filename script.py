236899be64dc584b04d9e501b081effffe71d8a0
Himanshu40 <himanshuwindows8.1@gmail.com>
2020-06-14 05:34:23 +0530

    Added test objects

---
+++ script.py
@@ -0,0 +1,8 @@
+# Project : The Boredless Tourist
+# Date    : Sun 14 Jun 2020 05:28:59 AM IST
+
+
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+
+test_travler = ["Erin Wilkes", "Shanghai, China", ["historical site", "art"]]
+
