f271aed1c0556ea5ccae607f10491d682c6fbae4
Himanshu40 <himanshuwindows8.1@gmail.com>
2020-06-14 07:03:49 +0530

    Added logic to find traveler destinations and convert to internal data

--- script.py
+++ script.py
@@ -4,5 +4,15 @@
 
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
 
-test_travler = ["Erin Wilkes", "Shanghai, China", ["historical site", "art"]]
+test_traveler = ["Erin Wilkes", "Shanghai, China", ["historical site", "art"]]
 
+# Retrieve the index of the traveler
+def get_destination(destination):
+    destination_index = destinations.index(destination)
+    return destination_index
+
+# Retrieve the index of test traveler by matching with destinations
+def get_traveler_location(traveler):
+    traveler_destination       = traveler[1]
+    traveler_destination_index = get_destination(traveler_destination)
+    return traveler_destination_index
